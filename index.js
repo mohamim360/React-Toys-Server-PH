54a9b2ef6077c04b84ed98eabd464a38b4c1cc87
mohamim360 <mohamim360@gmail.com>
2023-06-12 03:33:25 +0600

    get data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
+const port = process.env.PORT || 3000;
+const data = require('./data.json')
+const cors = require('cors');
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/data', (req, res) => {
+  res.send(data)
 })
 
 app.listen(port, () => {
