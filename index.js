c3db80839a15caeb8c845d861dd22b646fe1319e
mohamim360 <mohamim360@gmail.com>
2023-06-17 19:06:50 +0600

    remove env

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/data', (req, res) => {
 
 
 
-const uri = "mongodb+srv://Toy:ntOn5n38I3TTR6Nt@cluster0.dtd3sv1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtd3sv1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
